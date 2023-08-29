836a312b4050be2cb41b144f7e255c406b0d740c
Łukasz Kałużny <kaluza.docs@gmail.com>
2023-08-29 13:29:55 +0200

    Witchcorp image & favicon

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,9 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template lang="pug">
-header Hello i'm the header
-RouterView
+//- header Hello i'm the header
+//- RouterView
+img(src="@/assets/corpo_witches_3.png")
 </template>
 
 <style scoped>
